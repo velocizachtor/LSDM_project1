6c24afd4117e4c75d7b589bf0123c9e87b17bea4
velocizachtor <33331598+velocizachtor@users.noreply.github.com>
2019-01-14 20:05:44 -0800

    Add files via upload
    Question 1 finished, but the graph needs to be formatted with titles and better axis labels.

---
+++ Project_1.py
@@ -0,0 +1,28 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.datasets import fetch_20newsgroups
+# Refer to the offcial document of scikit-learn for detailed usages:
+# http://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_20newsgroups.html
+categories = ['comp.graphics', 'comp.sys.mac.hardware']
+# The
+twenty_train = fetch_20newsgroups(subset='train', # choose which subset of the dataset to use; can be 'train', 'test', 'all'
+                                  categories=None, # choose the categories to load; if is `None`, load all categories
+                                  shuffle=True,
+                                  random_state=42, # set the seed of random number generator when shuffling to make the outcome repeatable across different runs
+#                                   remove=['headers'],
+                                 )
+twenty_test = fetch_20newsgroups(subset='test', categories=None, shuffle=True, random_state=42)
+
+from pprint import pprint
+pprint(list(twenty_train.target_names))
+print(len(twenty_train.target))
+print(twenty_train.target[8])
+print(twenty_train.target[1423])
+
+
+#****************** Question 1 ****************************************************************************8
+
+plt.hist(twenty_train.target, bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+plt.show();
